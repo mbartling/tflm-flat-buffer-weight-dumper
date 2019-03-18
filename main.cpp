81a48cc63f8345279f8bdf729623bd391db53373
Michael Bartling <michael.bartling@arm.com>
2019-03-18 09:23:49 -0500

    Yay hacks

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+// Dont just my gross hacky code
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <cassert>
 
@@ -8,55 +10,81 @@
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
+#include "tensorflow/lite/core/api/flatbuffer_conversions.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
 
 
 using namespace std;
 
-template<typename T> dump_data(T* data, int len){
+template<typename T> 
+void dump_data(T* data, int len){
+    // Eww
+    // Only print out linear representation cuz that's all I need
+    cout << "~~~[" << endl;
     for(int i = 0; i < len; i++)
         cout << data[i] << endl;
+    cout << "~~~]" << endl;
 }
 
-void DumpWeights(TfLiteTensor* tensor){
+void DumpWeights(const tflite::Tensor* tensor,
+    const flatbuffers::Vector<flatbuffers::Offset<tflite::Buffer>>* flat_buffers){
+    void* tensor_data = nullptr;
+    int numElems = 0;
+    //if (auto* buffer = tensor->buffer()) {
+    if (auto* buffer = (*flat_buffers)[tensor->buffer()]) {
+        if (auto* array = buffer->data()) {
+            if (numElems = array->size()) {
+                cerr << numElems << endl;
+                tensor_data =
+                    const_cast<char*>(reinterpret_cast<const char*>(array->data()));
+            }
+        }
+    }
+    if(tensor_data == nullptr)
+        return;
 
-    /**
-      kTfLiteNoType = 0,
-      kTfLiteFloat32 = 1,
-      kTfLiteInt32 = 2,
-      kTfLiteUInt8 = 3,
-      kTfLiteInt64 = 4,
-      kTfLiteString = 5,
-      kTfLiteBool = 6,
-      kTfLiteInt16 = 7,
-      kTfLiteComplex64 = 8,
-      kTfLiteInt8 = 9,
-      */
-    int numElems = GetTensorShape(tensor);
-    switch (tensor.type){
-        case kTfLiteFloat32:
-            auto data = GetTensorData<float>(tensor);
-            dump_data(data, numElems);
-            break;
-        case kTfLiteInt32:
-            auto data = GetTensorData<int32_t>(tensor);
-            dump_data(data, numElems);
-            break;
-        case kTfLiteUInt8:
-            auto data = GetTensorData<uint8_t>(tensor);
-            dump_data(data, numElems);
-            break;
-        case kTfLiteInt64:
-            auto data = GetTensorData<int64_t>(tensor);
-            dump_data(data, numElems);
-            break;
-        case kTfLiteInt16:
-            auto data = GetTensorData<int16_t>(tensor);
-            dump_data(data, numElems);
-            break;
-        case kTfLiteInt8:
-            auto data = GetTensorData<int16_t>(tensor);
-            dump_data(data, numElems);
-            break;
+    switch (tensor->type()){
+        case tflite::TensorType_FLOAT32:
+            {
+                auto* data1 = reinterpret_cast<float*>(tensor_data);
+                dump_data(data1, numElems/sizeof(float));
+                break;
+            }
+        case tflite::TensorType_INT32:
+            {
+                auto* data2 = reinterpret_cast<int32_t*>(tensor_data);
+                dump_data(data2, numElems/sizeof(int32_t));
+                break;
+            }
+        case tflite::TensorType_UINT8:
+            {
+                auto* data3 = reinterpret_cast<uint8_t*>(tensor_data);
+                cout << hex;
+                dump_data(data3, numElems);
+                cout << dec;
+                break;
+            }
+        case tflite::TensorType_INT64:
+            {
+                auto* data4 = reinterpret_cast<int64_t*>(tensor_data);
+                dump_data(data4, numElems/sizeof(int64_t));
+                break;
+            }
+        case tflite::TensorType_INT16:
+            {
+                auto* data5 = reinterpret_cast<int16_t*>(tensor_data);
+                dump_data(data5, numElems/sizeof(int16_t));
+                break;
+            }
+        case tflite::TensorType_INT8:
+            {
+                cout << hex;
+                auto* data6 = reinterpret_cast<int8_t*>(tensor_data);
+                cout << dec;
+                dump_data(data6, numElems);
+                break;
+            }
     }
 };
 int main(int argc, char* argv[]) {
@@ -66,7 +94,7 @@ int main(int argc, char* argv[]) {
 
     assert(argc == 2);
     const char* tfile = argv[1];
-    cout << tfile << endl;
+    cerr << tfile << endl;
 
     // Read toco flatbuffer into mem
     std::ifstream file(tfile, std::ios::binary | std::ios::ate);
@@ -82,7 +110,7 @@ int main(int argc, char* argv[]) {
     // Map the model into a usable data structure. This doesn't involve any
     // copying or parsing, it's a very lightweight operation.
     const tflite::Model* model =
-        ::tflite::GetModel(g_tiny_conv_micro_features_model_data);
+        ::tflite::GetModel(buffer.data());
     if (model->version() != TFLITE_SCHEMA_VERSION) {
         error_reporter->Report(
                 "Model provided is schema version %d not equal "
@@ -91,22 +119,28 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    const flatbuffers::Vector<flatbuffers::Offset<Buffer>>* buffers =
+    const flatbuffers::Vector<flatbuffers::Offset<tflite::Buffer>>* buffers =
         model->buffers();
     auto* subgraphs = model->subgraphs();
 
     if (subgraphs->size() != 1) {
-        micro_error_reporter->Report("Only 1 subgraph is currently supported.\n");
-        return;
+        error_reporter->Report("Only 1 subgraph is currently supported.\n");
+        return -1;
     }
 
     auto subgraph_ = (*subgraphs)[0];
     auto tensors_ = subgraph_->tensors();
 
-    for (int i = 0; i < subgraph_->inputs()->Length(); ++i) {
-        const int tensor_index = subgraph_->inputs()->Get(i);
-        const auto* tensor = tensors_->Get(tensor_index);
-
+   // for (int i = 0; i < subgraph_->inputs()->Length(); ++i) {
+   //     const int tensor_index = subgraph_->inputs()->Get(i);
+   //     const auto* tensor = tensors_->Get(tensor_index);
+   //     DumpWeights(tensor, buffers);
+   // }
+    for (int i = 0; i < tensors_->Length(); ++i) {
+        const auto* tensor = tensors_->Get(i);
+        DumpWeights(tensor, buffers);
     }
+
+    return 0;
 
 }
